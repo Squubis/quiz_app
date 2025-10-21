62ec9d72b37280647852555c4f699b60280e34c2
Shlomie Garcia <shlomiegarcia@gmail.com>
2025-10-20 20:20:16 -0400

    Created structure to save answers. Created methos to save data to list. Modified questions_screen to utilize this function.

--- lib/questions_screen.dart
+++ lib/questions_screen.dart
@@ -2,7 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:quiz_app/models/answer_button.dart';
 import 'package:quiz_app/data/questions.dart';
 class QuestionsScreen extends StatefulWidget{
-  const QuestionsScreen({super.key});
+  const QuestionsScreen({super.key, required this.onSelectedAnswer});
+  final void Function(String answer) onSelectedAnswer;
 
   @override
   State<QuestionsScreen> createState() {
@@ -11,9 +12,17 @@ class QuestionsScreen extends StatefulWidget{
 }
 
 class _QuestionsScreenState extends State<QuestionsScreen>{
+  var currentQuestionIndex = 0;
+
+  void answerQuestion(String selectedAnswer){
+    widget.onSelectedAnswer(selectedAnswer);
+    setState((){
+      currentQuestionIndex++;
+    });
+  }
   @override
   Widget build(BuildContext context) {
-    final currentQuestion = questions[0];
+    final currentQuestion = questions[currentQuestionIndex];
     
     return SizedBox(
       width: double.infinity,
@@ -34,7 +43,7 @@ class _QuestionsScreenState extends State<QuestionsScreen>{
           ...currentQuestion.getShuffledAnswers().map((item){ //... is a forloop that basically adds a new list into the old list, which allows us to get a list of answer buttons to be injected into the screen properly
             return AnswerButton(
               answerText: item, 
-              onTap: (){});
+              onTap:(){answerQuestion(item);});
           }
           
           )
